5da3db351d4326d9047426ecf794985b4f278fae
Davit Ghazaryan <davit.ghazaryan@student.htldornbirn.at>
2025-03-14 14:45:06 +0100

    arrayMap 14.03.2025

---
+++ JS/Exerccises/arrayMap.js
@@ -0,0 +1,17 @@
+function square(number) {
+    return number ^ 2;    ////////////// number * number = number ^2
+}
+
+let numbers = [37, 56, 14];
+
+console.log("Squared Numbers:", squareNumbers);
+
+squareNumbers = [];
+for (let i = 0; i < numbers.lenght; i++) {
+    let sq = printNumber(numbers[i]);
+}
+
+console.log("Squared Numbers:", squareNumbers);
+
+
+
