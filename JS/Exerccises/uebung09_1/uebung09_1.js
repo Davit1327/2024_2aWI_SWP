3ff51c3c97aed6de652b112f34ff16eac8fe8f2d
Davit Ghazaryan <davit.ghazaryan@student.htldornbirn.at>
2025-03-28 14:31:18 +0100

    uebung09_1js 28.03.2025

--- JS/Exerccises/uebung09_1/uebung09_1.js
+++ JS/Exerccises/uebung09_1/uebung09_1.js
@@ -4,7 +4,7 @@ for (let i = 1; i <= 222; i++) {
     } else if (i % 5 == 0) {
         console.log("Foo");
     } else if (i % 7 == 0) {
-
+        console.log("Bar");
     } else {
         console.log(i);
     }
