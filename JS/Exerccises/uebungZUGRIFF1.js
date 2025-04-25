61dc078f4a7a3aec998462cc4bc202544379d8d4
Davit Ghazaryan <davit.ghazaryan@student.htldornbirn.at>
2025-04-25 14:04:55 +0200

    update

---
+++ JS/Exerccises/uebungZUGRIFF1.js
@@ -0,0 +1,15 @@
+
+
+let element = document.getElementById("content");
+element.innerHTML = "Liam und ich haben heute Döner gegesen";
+console.log(element);
+
+// Kürzer//
+
+document.getElementById("content").innerHTML = "Liam und ich haben heute Döner gegesen";
+
+
+
+
+
+
