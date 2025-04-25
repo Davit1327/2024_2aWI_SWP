d7313a3dfd94124b4019d1bfaedc844f4b1f36dc
Davit Ghazaryan <davit.ghazaryan@student.htldornbirn.at>
2025-04-25 14:47:28 +0200

    finished uebungZUGRIFF2 25.04.2025

--- JS/Exerccises/uebungZUGRIFF2.js
+++ JS/Exerccises/uebungZUGRIFF2.js
@@ -2,5 +2,22 @@
 
 
 function calculateSum() {
-    console.log("Calculete Clicked")
+    console.log("Calculete Clicked");
+
+    inputA = document.getElementById("inputA").value;
+    inputB = document.getElementById("inputB").value;
+
+    console.log("inputA: " + inputA);
+    console.log("inputB: " + inputB);
+
+    let result = Number(inputA) + Number(inputB);
+
+    console.log(typeof inputA);
+    console.log(typeof inputA);
+
+    console.log(result);
+
+    let resultElement = document.getElementById("result")
+    resultElement.innerHTML = result;
 }
+
