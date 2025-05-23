9ebfb4483463dfe2b1d19970a8da5e53aff15b52
Davit Ghazaryan <davit.ghazaryan@student.htldornbirn.at>
2025-05-23 13:51:02 +0200

    finish uebung11_01

--- JS/Exerccises/uebung11_01/uebung11_01.js
+++ JS/Exerccises/uebung11_01/uebung11_01.js
@@ -1,1 +1,20 @@
-let input = "ATGCTTCAGAAAAGGTCAGCG";
+let IN = "ATGCTTCAGAAAAGGTCAGCG";
+
+let A = 0;
+let C = 0;
+let G = 0;
+let T = 0;
+
+if (IN.length > 0 && IN.length < 1000) {
+    for (let i = 0; i <= IN.length; i++) {
+        if (IN[i] == "A") {
+            A += 1;
+        } else if (IN[i] == "C") {
+            C += 1;
+        } else if (IN[i] == "G") {
+            G += 1;
+        } else if (IN[i] == "T") {
+            T += 1;
+        }
+    } console.log(A, C, G, T);
+}
