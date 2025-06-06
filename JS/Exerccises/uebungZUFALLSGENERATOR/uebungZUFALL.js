9e5db18e2e3e5fc50f8e0d5b21fd53f415c4292b
Davit Ghazaryan <davit.ghazaryan@student.htldornbirn.at>
2025-06-06 14:32:17 +0200

    finished uebungZUFALL

--- JS/Exerccises/uebungZUFALLSGENERATOR/uebungZUFALL.js
+++ JS/Exerccises/uebungZUFALLSGENERATOR/uebungZUFALL.js
@@ -1,13 +1,12 @@
-function generate_header() {
-
-}
-
-
-
 function generate() {
     let num1 = document.getElementById("inA").value;
     let num2 = document.getElementById("inB").value;
 
+    let diff = parseInt(num2) - parseInt(num1);
 
+    let randomNum = Math.floor(Math.random() * diff) + parseInt(num1) + 1;
+    console.log(randomNum);
+
+    document.getElementById("randomNum").innerHTML = randomNum;
 
 }
