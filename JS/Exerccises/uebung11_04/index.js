e2267e3a033c405288e0190fc71f3e92681ff761
Davit Ghazaryan <davit.ghazaryan@student.htldornbirn.at>
2025-06-13 14:47:32 +0200

    uebung11_04

---
+++ JS/Exerccises/uebung11_04/index.js
@@ -0,0 +1,20 @@
+let p = 20;
+let h = 100;
+let r = 600;
+
+
+let salary = p * h;
+
+
+console.log(salary);
+
+
+if (salary > r) {
+    console.log("YES");
+}
+else if (salary < r) {
+    console.log("NO");
+}
+else {
+    console.log("BARELY");
+}
